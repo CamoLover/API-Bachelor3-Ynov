07d6257474aff21bd6b10628f8295df9c6bc79db
7cMyrzen <7c.myrzen@gmail.com>
2025-11-13 22:10:52 +0100

    Page statique nextjs

---
+++ client-nextjs/app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "SachAPI",
+  description: "API officielle sur Sacha",
+  icons: {
+    icon: "/sacha-logo.png",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased h-screen w-screen`}
+      >
+        <div className="fixed inset-0 bg-[url('/sacha-background.jpeg')] bg-cover bg-center blur-sm z-0 opacity-30" />
+        {children}
+      </body>
+    </html>
+  );
+}
